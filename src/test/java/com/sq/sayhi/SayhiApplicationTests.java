fa9415e75f7777e9079cbe6fb3343feb7a2e0bab
zhangxw_ <zhangxw_j@126.com>
2020-11-10 17:21:47 +0800

    init

---
+++ src/test/java/com/sq/sayhi/SayhiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sq.sayhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SayhiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
